09c235f84577cd9619717384edf2a34780daf40f
Vince <vlee9527@gmail.com>
2022-08-05 03:17:59 +0800

    rwd dash complete

--- client/src/components/ChatContainer.js
+++ client/src/components/ChatContainer.js
@@ -2,31 +2,38 @@ import React, { useState } from "react";
 import MatchesDisplay from "./MatchesDisplay";
 import ChatHeader from "./ChatHeader";
 import ChatDisplay from "./ChatDisplay";
+import chatExtendLogo from "../images/message-circle.svg";
 
 const ChatContainer = ({ user }) => {
   const [clickedUser, setClickedUser] = useState(null);
 
   return (
-    <div className="chat-container">
-      <ChatHeader user={user} />
-      <div className="chat-option-selector">
-        <button
-          className={`option ${clickedUser ? "notActive" : ""}`}
-          onClick={() => setClickedUser(null)}
-        >
-          Matches
-        </button>
-        <button className={`option-chat ${clickedUser ? "" : "notActive"}`}>
-          Chat
-        </button>
+    <div className="chat-section">
+      <input type="checkbox" class="chat-toggle" id="chat-toggle"></input>
+      <label for="chat-toggle" class="chat-toggle-label">
+        <img src={chatExtendLogo} />
+      </label>
+      <div className="chat-container">
+        <ChatHeader user={user} />
+        <div className="chat-option-selector">
+          <button
+            className={`option ${clickedUser ? "notActive" : ""}`}
+            onClick={() => setClickedUser(null)}
+          >
+            Matches
+          </button>
+          <button className={`option-chat ${clickedUser ? "" : "notActive"}`}>
+            Chat
+          </button>
+        </div>
+        {!clickedUser && (
+          <MatchesDisplay
+            matches={user.matches}
+            setClickedUser={setClickedUser}
+          />
+        )}
+        {clickedUser && <ChatDisplay clickedUser={clickedUser} user={user} />}
       </div>
-      {!clickedUser && (
-        <MatchesDisplay
-          matches={user.matches}
-          setClickedUser={setClickedUser}
-        />
-      )}
-      {clickedUser && <ChatDisplay clickedUser={clickedUser} user={user} />}
     </div>
   );
 };
